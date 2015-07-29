dc51959f039c6af1607eab7522f96ec57f518579
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2015-07-29 12:48:58 +0530

    Ignore vendor/bundle

--- .init.sh
+++ .init.sh
@@ -5,6 +5,6 @@ set -e
 gem which bundler >/dev/null 2>&1 || (echo "Installing bundler"; gem install bundler)
 
 export NOKOGIRI_USE_SYSTEM_LIBRARIES=1
-bundle check > /dev/null || bundle install --path vendor/bundle --binstubs --clean
+bundle check > /dev/null || bundle install --path vendor/bundle --binstubs --clean --local
 
 echo "Done."
