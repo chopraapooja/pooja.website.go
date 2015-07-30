23381b266fa61dbb747bfc305282b5076122a561
Aravind SV <arvind.sv@gmail.com>
2015-07-30 10:55:56 +0530

    Section 2 - Draft.
    Should have information about support features and add-ons later.

--- source/assets_site/javascripts/index.js
+++ source/assets_site/javascripts/index.js
@@ -1,1 +1,58 @@
-(function () {}());
+(function () {
+  // bail on mobile
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    return;
+  }
+
+  var $cards = $('.cards'),
+    $images = $cards.find('.images'),
+    $window = $(window);
+
+  var cachePosition = false;
+
+  function reposition() {
+    var cardTop = $cards.position().top,
+      cardHeight = $cards.outerHeight();
+      cardBottom = cardTop + cardHeight,
+      singleCardHeight = $cards.find('.card').outerHeight(),
+      $image = $images.find('img'),
+      imageHeight = $image.height(),
+      windowHeight = $window.height(),
+      scrollTop = $window.scrollTop(),
+      imageOffset = 0,
+      percentageOfScreenWhenImageHasToStopScrolling = 0.5;
+
+    if (scrollTop + windowHeight * percentageOfScreenWhenImageHasToStopScrolling + imageOffset < cardTop + singleCardHeight/2) { // above
+      cachePosition = false;
+      $images.css({
+        position: 'absolute',
+        top: singleCardHeight / 2 - imageHeight / 2,
+        bottom: ''
+      });
+    } else if (scrollTop + windowHeight * percentageOfScreenWhenImageHasToStopScrolling + imageOffset > cardBottom - singleCardHeight/2) { // below
+      cachePosition = false;
+      $images.css({
+        position: 'absolute',
+        top: '',
+        bottom: singleCardHeight / 2 - imageHeight / 2
+      });
+    } else { // between
+      if (!cachePosition) {
+        $images.css({
+          position: 'fixed',
+          top: windowHeight * percentageOfScreenWhenImageHasToStopScrolling - imageHeight/2 + imageOffset
+        });
+
+        cachePosition = true;
+      }
+    }
+  }
+
+  $window.on('scroll', function() {
+    reposition();
+  });
+  $window.on('resize', function() {
+    cachePosition = false;
+    reposition();
+  });
+}());
